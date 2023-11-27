96df197c86526c7a3868641af3086ed29ff59489
kmkkkp <kmkkkp@ajou.ac.kr>
2023-11-28 02:42:19 +0900

    fix: address 고침

--- lib/service/versioncheck.dart
+++ lib/service/versioncheck.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as htmlParser;
 
 Future<String?> fetchServerAppVersion() async {
-  final url = Uri.parse('https://api.weconnect.support/getVersion');
+  final url = Uri.parse('https://api-dev.weconnect.support/getVersion');
   final response = await http.get(url); // 서버 엔드포인트를 여기에 입력하세요.
   if (response.statusCode == 200) {
     final data = json.decode(response.body);
